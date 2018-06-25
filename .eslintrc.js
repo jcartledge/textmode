3ddb6ec45c594afcc48e8f62c6cfc78095358f44
James Cartledge <jcartledge@gmail.com>
2018-06-25 21:37:44 +1000

    Added tests and docs.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
     "extends": "semistandard",
     "env": {
         "browser": true
+    },
+    "rules": {
+        "valid-jsdoc": 2
     }
 };
--- test/textMode.test.js
+++ test/textMode.test.js
@@ -1,15 +1,35 @@
 import test from 'tape';
-import {TextMode} from '../lib/textMode.js';
+import {TextMode, TextModeFont, textModePalette} from '../lib/textMode.js';
 
 test('Constructor with default args creates a canvas', t => {
+  t.plan(1);
   const tm = new TextMode();
   t.ok(tm.canvas instanceof HTMLCanvasElement);
-  t.end();
 });
 
 test('Constructor passed a canvas uses it', t => {
+  t.plan(1);
   const canvas = document.createElement('canvas');
   const tm = new TextMode({canvas});
   t.equal(canvas, tm.canvas);
-  t.end();
+});
+
+test('Constructor with default args has a font', t => {
+  t.plan(1);
+  const tm = new TextMode();
+  t.ok(tm.font instanceof TextModeFont);
+});
+
+test('Constructor passed a font uses it', t => {
+  t.plan(1);
+  class TestFont extends TextModeFont {}
+  const font = new TestFont();
+  const tm = new TextMode({font});
+  t.equal(tm.font, font);
+});
+
+test('Constructor with default args has a palette', t => {
+  t.plan(1);
+  const tm = new TextMode();
+  t.equal(tm.palette, textModePalette);
 });
